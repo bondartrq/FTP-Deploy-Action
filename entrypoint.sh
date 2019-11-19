a97d1d462c9eb6d88d7bb4ba4187b94bc285c872
Roberto <bondartrq@gmail.com>
2019-11-19 16:50:29 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,7 @@ echo "Using $WDEFAULT_METHOD to connect to port $WDEFAULT_PORT"
 echo "Uploading files..."
 
 # lftp -u $FTP_USERNAME,$FTP_PASSWORD $WDEFAULT_METHOD://$FTP_SERVER:$WDEFAULT_PORT -e "set ftp:ssl-allow no; mirror $WDEFAULT_ARGS -R $WDEFAULT_LOCAL_DIR $WDEFAULT_REMOTE_DIR; quit"
-lftp -u $FTP_USERNAME,$FTP_PASSWORD sftp://$FTP_SERVER -e "set ftp:ssl-allow no; mirror --delete --verbose -R prueba prueba; quit"
+lftp -u $FTP_USERNAME,$FTP_PASSWORD sftp://$FTP_SERVER -e "set ftp:ssl-allow no; mirror --delete --verbose -R prueba html/prueba; quit"
 
 
 echo "FTP Deploy Complete"
